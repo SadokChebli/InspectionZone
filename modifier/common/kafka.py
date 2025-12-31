5f0bf953b8fbea7c62dd0e22c779af10d21bb26e
Adim Boubaker <adim.boubaker@etudiant-enit.utm.tn>
2025-12-31 12:00:33 +0100

    version that works

--- modifier/common/kafka.py
+++ modifier/common/kafka.py
@@ -1,12 +1,14 @@
 import json
 from kafka import KafkaProducer, KafkaConsumer
 
-BOOTSTRAP = "kafka:9092"
+BOOTSTRAP = "kafka-svc.inspection-zone.svc.cluster.local:9092"
 
 def get_producer():
     return KafkaProducer(
         bootstrap_servers=BOOTSTRAP,
-        value_serializer=lambda v: json.dumps(v).encode()
+        value_serializer=lambda v: json.dumps(v).encode(),
+        # AJOUTER CETTE LIGNE :
+        api_version=(3, 4, 0) 
     )
 
 def get_consumer(topic, group_id):
@@ -15,5 +17,7 @@ def get_consumer(topic, group_id):
         bootstrap_servers=BOOTSTRAP,
         group_id=group_id,
         value_deserializer=lambda v: json.loads(v.decode()),
-        auto_offset_reset="earliest"
+        auto_offset_reset="earliest",
+        # AJOUTER CETTE LIGNE :
+        api_version=(3, 4, 0)
     )
